a789077218327cd36e5ae05cce31c30534f90f66
oscarmegia <oscar.megia@gmail.com>
2019-08-16 17:06:45 +0200

    más mocks

---
+++ src/mocks/noticias/feed-noticias.js
@@ -0,0 +1,9 @@
+module.exports= (function(){
+    let map=new Map()
+    map.set('elmundo','https://e00-elmundo.uecdn.es/elmundo/rss/portada.xml')
+    map.set('abc','https://www.abc.es/rss/feeds/abcPortada.xml')
+    map.set('20minutos','https://www.20minutos.es/rss/')
+    map.set('elpais','http://ep00.epimg.net/rss/tags/ultimas_noticias.xml')
+    return map
+})()
+
--- src/server/components/index.vue
+++ src/server/components/index.vue
@@ -1,7 +1,9 @@
 <template >
 <div class='main'>
              <div class='mongo-data' v-on:click='textToSpeech()'> 
-                 {{io_mongo.fullDocument._id}} | {{io_mongo.fullDocument.fecha}}
+                 <!-- {{io_mongo.fullDocument._id}} | {{io_mongo.fullDocument.fecha}} -->
+                 <p>{{io_mongo.fullDocument.title}}</p>
+                 
                  
             </div>
   </div>
@@ -10,6 +12,7 @@
     var available_voices;
     var textoActual=''
     var english_voice = '';
+    var texto_leido = true;
     Array.prototype.clear = function() {
     this.splice(0, this.length);
     };
@@ -20,7 +23,8 @@
             },
             mongo: function(data){
                 this.io_mongo=data
-                this.textoActual = this.io_mongo.fullDocument.fecha
+                this.textoActual = this.io_mongo.fullDocument.title
+                this.textToSpeech()
             }
 
         },
@@ -29,7 +33,8 @@
                 io_mongo:{
                     fullDocument:{
                         _id:'',
-                        fecha:''
+                        fecha:'',
+                        title:''
                     }
                 },
                 utter:{}
@@ -37,14 +42,20 @@
         },
         methods:{
                 textToSpeech: function( ){
-                    console.log("click")
-                    this.utter = new SpeechSynthesisUtterance();
-                    this.utter.rate = 1;
-                    this.utter.pitch = 0.5;
+                    console.log('$$$',texto_leido)
+                    // if(texto_leido){
+                        
+                                        console.log("click")
+                                        this.utter = new SpeechSynthesisUtterance();
+                                        this.utter.rate = 1;
+                                        this.utter.pitch = 0.5;
+                                        
+                                        this.utter.voice = english_voice;
+                                        this.utter.text = this.textoActual;
+                                        window.speechSynthesis.speak(this.utter);
+                                        texto_leido=false;
+                    // }
                     
-                    this.utter.voice = english_voice;
-                    this.utter.text = this.textoActual;
-                    window.speechSynthesis.speak(this.utter);
                 }
         },
         created(){
@@ -66,7 +77,7 @@
                 this.utter.voice = english_voice;
                 // event after text has been spoken
                 this.utter.onend = function() {
-                //alert('Speech has finished');
+                    texto_leido=true;
                 }
         },
         
